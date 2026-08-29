a0488829d107383af268a062e0249411c5ef337c
gorkagg10 <gorka.gonzalez.gomez@gmail.com>
2026-08-29 18:52:17 +0200

    feat: first commit

---
+++ cmd/api/server.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	chimw "github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	router := chi.NewRouter()
+	router.Use(chimw.RequestID)
+	router.Use(chimw.Recoverer)
+
+	srv := &http.Server{
+		Addr:         ":8080",
+		Handler:      router,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  60 * time.Second,
+	}
+
+	go func() {
+		slog.Info("server starting", "port", "8080")
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			slog.Error("server failed", "error", err)
+			os.Exit(1)
+		}
+	}()
+
+	<-ctx.Done()
+
+	err := shutdownServer(srv)
+	if err != nil {
+		slog.Error("graceful shutdown failed", slog.String("error", err.Error()))
+		os.Exit(1)
+	}
+}
+
+func shutdownServer(srv *http.Server) error {
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module equity-calculator-api
+
+go 1.24.3
+
+require github.com/go-chi/chi/v5 v5.3.2
